afcebd5b5283abe190d61ecf07a700c4be6743e9
Kerolos2000 <kero.magdy2000@gmail.com>
2025-12-10 23:02:11 +0200

    feat: 🎸 add Separator form shadcn and prettier ui components

---
+++ src/components/ui/action-icon-button.tsx
@@ -0,0 +1,19 @@
+import { HTMLMotionProps, motion } from "framer-motion";
+import { ReactNode } from "react";
+
+interface ActionIconButtonProps extends HTMLMotionProps<"button"> {
+  children: ReactNode;
+}
+
+export function ActionIconButton({ children, ...rest }: ActionIconButtonProps) {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+      className="w-9 h-9 rounded-full bg-white/90 backdrop-blur-sm flex items-center justify-center shadow-md hover:bg-white cursor-pointer"
+      {...rest}
+    >
+      {children}
+    </motion.button>
+  );
+}
