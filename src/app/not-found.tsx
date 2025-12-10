3885728183d7f3e6ca94c7ee640ae4b80bd6a838
Kerolos2000 <kero.magdy2000@gmail.com>
2025-12-10 16:57:49 +0200

    feat: 🎸 adding essential Shadcn components and not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center text-center">
+      <div>
+        <h2 className="text-9xl font-semibold md:text-[240px]">404</h2>
+        <p className="h2 font-semibold">Sorry! page did not found</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
